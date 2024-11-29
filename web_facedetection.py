1823daf6a092edcbe1c03c85ac95b64557e7767f
Jay M <60815668+jaymoundekar18@users.noreply.github.com>
2024-11-29 12:16:28 +0530

    Update web_facedetection.py

--- web_facedetection.py
+++ web_facedetection.py
@@ -9,6 +9,10 @@ import deepface
 st.title('Face App')
 
 st.subheader("\n\n\n\n")
+
+st.write("DeepFace Version:", deepface.__version__)
+st.write("DeepFace Directory:", dir(deepface))
+st.write("Python Path:", sys.path)
 st.write(deepface.__version__)
 st.subheader(dir(deepface))
 
